f40cf5a1b9b892cadd57810911fdc8419bc62848
akshaylg0314 <akshay14.garg@lge.com>
2026-04-03 18:29:55 +0530

    Deleting Old lifecycle code

--- Node1/lifecycle/scripts/generate_cpp_compile_commands.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bazel run @hedron_compile_commands//:refresh_all
--- Node1/lifecycle/scripts/generate_rust_analyzer_support.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Manual targets are not take into account, must be set explicitly
-bazel run @rules_rust//tools/rust_analyzer:gen_rust_project
